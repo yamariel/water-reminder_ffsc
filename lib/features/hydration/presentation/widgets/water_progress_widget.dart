ea9a4e56f904cfbaa42e19f7f449415bc8880999
Abdoul Kader ILBOUDO <ikader37@192.168.1.164>
2026-09-06 19:49:41 +0000

    feat: creer le widget de lentete et celui de la goutte d'eau

---
+++ lib/features/hydration/presentation/widgets/water_progress_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class WaterProgressWidget extends StatelessWidget{
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}

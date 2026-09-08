f4df7d895beaa8095f4dfbbffe4a6eb05702b397
Abdoul Kader ILBOUDO <ikader37@Abdouls-MacBook-Pro.local>
2026-09-08 22:53:12 +0000

    feat: realiser les animations du verre

---
+++ lib/features/hydration/presentation/widgets/verre_anime.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:water/features/hydration/presentation/widgets/peintre_verre_widget.dart';
+
+/**
+ * Ce widget gerera lanimation du verre
+ */
+class VerreAnime extends StatefulWidget {
+  final int objectifJournalier;
+  final int valuerConsommee;
+
+  VerreAnime({required this.objectifJournalier, required this.valuerConsommee});
+
+  @override
+  State<StatefulWidget> createState() => _VerreAnimeState();
+}
+
+/**
+ * Un Ticker agit comme le battement de cœur d'une animation :
+ * il déclenche un signal à chaque rafraîchissement d'écran de l'appareil (généralement 60 ou 120 fois par seconde)
+ * en transmettant le temps écoulé, ce qui permet de calculer le rendu image par image
+ * .
+ */
+class _VerreAnimeState extends State<VerreAnime>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late Animation<double> _animation;
+
+  /// Progression actuellement affichée par l'animation.
+  late double _ancienneProgression =
+      (widget.valuerConsommee / widget.objectifJournalier).clamp(0.0, 1.0);
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+      animationBehavior: AnimationBehavior.normal
+    );
+    _animation = Tween<double>(
+      begin: 0.0,
+      end: widget.valuerConsommee / widget.objectifJournalier,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+    _controller.forward();
+  }
+
+  /// Calcule la progression entre 0 et 1.
+  double _calculerProgression() {
+    if (widget.objectifJournalier <= 0) {
+      return 0.0;
+    }
+
+    return (widget.valuerConsommee / widget.objectifJournalier).clamp(0.0, 1.0);
+  }
+
+  /// Appelé lorsque les paramètres du widget changent.
+  @override
+  void didUpdateWidget(covariant VerreAnime oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.valuerConsommee != widget.valuerConsommee ||
+        oldWidget.objectifJournalier != widget.objectifJournalier) {
+      final double nouvelleProgression = _calculerProgression();
+
+      _animation = Tween<double>(
+        begin: _ancienneProgression,
+        end: nouvelleProgression,
+      ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+      _ancienneProgression = nouvelleProgression;
+
+      /// On recommence l'animation.
+      _controller
+        ..reset()
+        ..forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Column(
+      children: [
+        Text(
+          '${widget.valuerConsommee} ml',
+          style: theme.textTheme.labelMedium?.copyWith(
+            fontWeight: FontWeight.bold,
+            color: theme.colorScheme.onPrimaryContainer
+
+          ),
+        ),
+
+        AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return SizedBox(
+              width: double.maxFinite,
+              height: 300,
+
+              child: CustomPaint(
+                painter: PeintreVerreWidget(progression: _animation.value),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/features/hydration/presentation/widgets/water_progress_widget.dart
+++ lib/features/hydration/presentation/widgets/water_progress_widget.dart
@@ -1,39 +1,111 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:water/features/hydration/presentation/widgets/boutons_rapid_widget.dart';
 import 'package:water/features/hydration/presentation/widgets/echelle_gauche_widget.dart';
 import 'package:water/features/hydration/presentation/widgets/entete_widget.dart';
+import 'package:water/features/hydration/presentation/widgets/verre_anime.dart';
 import 'package:water/features/hydration/presentation/widgets/verre_widget.dart';
 
-class WaterProgressWidget extends StatelessWidget{
+class WaterProgressWidget extends StatefulWidget {
   final int valuerConsommee;
   final int objectifJournalier;
 
-
   const WaterProgressWidget({
     required this.valuerConsommee,
-    required this.objectifJournalier
+    required this.objectifJournalier,
   });
 
   @override
+  State<WaterProgressWidget> createState() => _WaterProgressWidgetState();
+}
+
+class _WaterProgressWidgetState extends State<WaterProgressWidget> {
+  late int _valeurConsommee;
+  @override
+  void initState() {
+    super.initState();
+
+    _valeurConsommee = widget.valuerConsommee;
+  }
+
+  void _ajouterEau() {
+    setState(() {
+      _valeurConsommee += 250;
+
+      // Empêche de dépasser l'objectif
+      if (_valeurConsommee > widget.objectifJournalier) {
+        _valeurConsommee = widget.objectifJournalier;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     //Calculer le procentage de progression
-    final double progression=(valuerConsommee/objectifJournalier).clamp(0.0, 1.0);
+
+    final double progression = (_valeurConsommee / widget.objectifJournalier)
+        .clamp(0.0, 1.0);
+
+    final int pourcentage = (progression * 100).round();
+
+    //final double progression=(valuerConsommee/objectifJournalier).clamp(0.0, 1.0);
     return SizedBox(
-      height: 350,
+      height: 450,
+      width: double.infinity,
+
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          /// ÉCHELLE
-          EchelleGaucheWidget(objectifJournalier:objectifJournalier),
+          /// ZONE DU VERRE
+          Expanded(
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 60,
+                  child: EchelleGaucheWidget(
+                    objectifJournalier: widget.objectifJournalier,
+                  ),
+                ),
+                Expanded(
+                  child: VerreAnime(
+                    objectifJournalier: widget.objectifJournalier,
+                    valuerConsommee: _valeurConsommee,
+                  ),
+                ),
+              ],
+            ),
+          ),
 
-
-          /// VERRE
-          VerreWidget(progression: valuerConsommee/objectifJournalier, valuerConsommee: valuerConsommee),
+          /// ZONE DU BAS
+          // Column(
+          //   mainAxisSize: MainAxisSize.max,
+          //   children: [
+          //     Text(
+          //       '$pourcentage%',
+          //       style: const TextStyle(
+          //         fontSize: 24,
+          //         fontWeight: FontWeight.bold,
+          //       ),
+          //     ),
+          //
+          //     // const Text('Progression', style: TextStyle(fontSize: 14)),
+          //
+          //     const SizedBox(height: 8),
+          //
+          //     // ElevatedButton(
+          //     //   onPressed: _ajouterEau,
+          //     //   child: const Text('+ 250 ml'),
+          //     // ),
+          //   ],
+          // ),
         ],
       ),
     );
   }
 
-
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
 }

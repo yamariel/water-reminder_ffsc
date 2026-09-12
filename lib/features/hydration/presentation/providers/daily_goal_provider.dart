b743903b2789fda5c0a40e1c5dd2dcaf0c7ef965
Sie Donald TRAORE <naldotraore688@gmail.com>
2026-09-12 20:35:01 +0000

    ''modif''

--- lib/features/hydration/presentation/providers/daily_goal_provider.dart
+++ lib/features/hydration/presentation/providers/daily_goal_provider.dart
@@ -3,4 +3,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 /// Provider temporaire pour l'objectif quotidien.
 /// TODO: remplacer par la valeur calculée dans settingsProvider
 
-final dailyGoalProvider = StateProvider<int>((ref) => 2550);
+class DailyGoalNotifier extends Notifier<int> {
+  @override
+  int build() => 2550;
+
+  void updateGoal(int newGoal) {
+    state = newGoal;
+  }
+}
+
+final dailyGoalProvider = NotifierProvider<DailyGoalNotifier, int>(() {
+  return DailyGoalNotifier();
+});

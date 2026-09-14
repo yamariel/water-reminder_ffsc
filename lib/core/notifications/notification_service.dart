3774b061c855195145335b439863ca1ac17b7f82
yamariel <ariel.yamien@epitech.eu>
2026-09-14 11:30:31 +0000

    feat(notifications): implémente le service de notifications locales

--- lib/core/notifications/notification_service.dart
+++ lib/core/notifications/notification_service.dart
@@ -1,3 +1,89 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
 class NotificationService {
-  static Future<void> init() async {}
+  final notificationsPlugin = FlutterLocalNotificationsPlugin();
+  bool _isInitialized = false;
+  bool get isInitialized => _isInitialized;
+
+  //initialisation des notifications
+  Future<void> init() async {
+    tz.initializeTimeZones();
+    final TimezoneInfo timeZoneName = await FlutterTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZoneName as String));
+
+    if (_isInitialized) return;
+
+    //préparer l'init du param android/iOS
+    const initSettingsAndroid = AndroidInitializationSettings(
+      '@mipmap/ic_launcher',
+    );
+    const initSettingsIOS = DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+    );
+
+    //init des params
+    const initSettings = InitializationSettings(
+      android: initSettingsAndroid,
+      iOS: initSettingsIOS,
+    );
+    await notificationsPlugin.initialize(settings: initSettings);
+    _isInitialized = true;
+  }
+  
+  Future<void> scheduleNotification({
+    required int id,
+    required String title,
+    required String body,
+    required DateTime scheduledTime,
+  }) async {
+    await notificationsPlugin.zonedSchedule(
+      id: id,
+      title: title,
+      body: body,
+      scheduledDate: tz.TZDateTime.from(scheduledTime, tz.local),
+      notificationDetails: notificationDetails(),
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+    );
+  }
+
+  //demander la permission d'envoyer des notifs
+  Future<void> requestPermissions() async {
+    await notificationsPlugin
+        .resolvePlatformSpecificImplementation<IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(alert: true, badge: true, sound: true);
+
+    await notificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestNotificationsPermission();
+  }
+
+  //détails des notifications
+  NotificationDetails notificationDetails() {
+    return const NotificationDetails(
+      android: AndroidNotificationDetails(
+        'daily_channel_id',
+        'Daily notifications',
+        channelDescription: 'Daily Notification',
+        importance: Importance.max,
+        priority: Priority.high,
+      ),
+      iOS: DarwinNotificationDetails(),
+    );
+  }
+
+
+  //affichage de la notification
+  Future<void> showNotification({int id = 0, String? title, String? body}) async {
+    return notificationsPlugin.show(
+      id: id,
+      title: title,
+      body: body,
+      notificationDetails: NotificationDetails(),
+    );
+  }
 }
